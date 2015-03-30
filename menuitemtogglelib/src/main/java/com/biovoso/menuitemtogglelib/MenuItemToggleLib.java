76a0c12665fb05b48f6d16fe314e6a61ea8aa830
MahirManghnani <hitit1@hotmail.com>
2015-03-30 23:02:20 +0100

    Added Apache licence

--- menuitemtogglelib/src/main/java/com/biovoso/menuitemtogglelib/MenuItemToggleLib.java
+++ menuitemtogglelib/src/main/java/com/biovoso/menuitemtogglelib/MenuItemToggleLib.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Mahir Manghnani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.biovoso.menuitemtogglelib;
 
 import android.animation.AnimatorSet;
@@ -66,8 +82,6 @@ public class MenuItemToggleLib extends RelativeLayout
         button_view = findViewById(R.id.button_view);
         item_1 = (ImageView) findViewById(R.id.icon_1);
         item_2 = (ImageView) findViewById(R.id.icon_2);
-
-        setClipChildren(false);
 
         button_view.setOnClickListener(new OnClickListener()
         {
